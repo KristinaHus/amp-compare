a0216fa25d663277f6c868f92a275fbfc67b72c5
kina <kina.girl@gmail.com>
2018-09-09 22:57:48 +0300

    Fix routes

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const path = require('path')
 
 const app = express()
 
-app.get('/assets', express.static(path.join(__dirname, 'assets')))
+app.use('/assets', express.static(path.join(__dirname, 'assets')))
 
 app.get('/standard', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'index.html'))
 })
-app.use('/amp', (req, res) => {
+app.get('/amp', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'amp-index.html'))
 })
-let server = app.listen(process.env.PORT || 5000, () => {
-  console.log('Server listening on port 5000')
+let server = app.listen(process.env.PORT || 3000, () => {
+  console.log('Server listening on port 3000')
 })
